f25a5a5f0b7d33192b70ce7432cdecb609ac43a6
hevelius <mario.perrotta@unimi.it>
2020-12-18 12:27:44 +0100

    added function to broadcast message to all connected clients

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 var net = require('net');
+var clients = [];
+
+const broadcast = (msg, sender) => {
+    clients.forEach((client) => {
+        if (client === sender) return;
+        client.write(msg);
+    });
+};
 
 var server = net.createServer(function(socket) {
     socket.write("Welcome " + socket.remoteAddress + ":" + socket.remotePort  + "\n");
+    clients.push(socket);
     socket.on('data', function (data) {
-        console.log(data);
+        broadcast(socket.remoteAddress + "> " + data, socket);
     });
     socket.on('end', () => {
-        console.log('end connection');
+        let index = clients.indexOf(socket);
+        if (index !== -1) {
+            clients.splice(index, 1);
+        }
     });
-
 });
 
 server.listen(process.env.PORT, '0.0.0.0');
